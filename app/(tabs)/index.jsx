0e50b2520c9260a8d56a18dc0f1aaed1b11e84f7
Meelo <peterbam2020@gmail.com>
2025-06-26 16:35:31 +0100

    LITTLE CHANGES

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 import { RFValue } from "react-native-responsive-fontsize";
 import {
   SafeAreaView,
@@ -18,92 +19,100 @@ export default function HomeScreen() {
   return (
     <ImageBackground
       source={require("../../assets/images/background.png")}
+      resizeMode="cover"
       style={{
-        resizeMode: "contain",
         alignItems: "center",
-        height: RFValue(641),
-        opacity: RFValue(10),
+        flex: 1,
+        opacity: 1,
       }}
     >
-      <SafeAreaView
-        style={{
-          flex: 1,
-          backgroundColor: "#3752F4",
-          justifyContent: "flex-start",
-          paddingTop: insets.top,
-          paddingBottom: insets.bottom,
-          paddingRight: insets.right,
-          paddingLeft: insets.left,
-        }}
+      <LinearGradient
+        colors={[
+          "rgba(55, 82, 244, 1)", // solid blue at the top
+          "rgba(55, 82, 244, 0.6)", // medium blue
+          "rgba(55, 82, 244, 0.2)", // faint at the bottom
+        ]}
+        style={{ flex: 1 }}
       >
-        <View
+        <SafeAreaView
           style={{
-            alignItems: "center",
-            alignSelf: "center",
-            marginTop: RFValue(40),
+            flex: 1,
+            justifyContent: "flex-start",
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingRight: insets.right,
+            paddingLeft: insets.left,
           }}
         >
-          <Image
-            style={{
-              marginTop: RFValue(10),
-              height: RFValue(100),
-              width: RFValue(122),
-              resizeMode: "contain",
-            }}
-            source={require("../../assets/images/icon.png")}
-          />
-          <Text
-            style={{
-              fontSize: RFValue(40),
-              fontWeight: "bold",
-              color: "white",
-            }}
-          >
-            driveit
-          </Text>
           <View
             style={{
+              alignItems: "center",
+              alignSelf: "center",
               marginTop: RFValue(40),
-              justifyContent: "space-between",
-              flexDirection: "column",
             }}
           >
-            <TextInput
+            <Image
               style={{
-                backgroundColor: "white",
-                width: RFValue(300),
-                height: RFValue(50),
-                borderRadius: RFValue(74),
-                paddingHorizontal: RFValue(30),
-                marginHorizontal: RFValue(100),
+                marginTop: RFValue(10),
+                height: RFValue(100),
+                width: RFValue(122),
+                resizeMode: "contain",
               }}
+              source={require("../../assets/images/icon.png")}
             />
-            <TextInput
+            <Text
               style={{
-                backgroundColor: "white",
-                width: RFValue(300),
-                height: RFValue(50),
-                marginTop: RFValue(30),
-                borderRadius: RFValue(74),
-                paddingHorizontal: RFValue(30),
-                marginHorizontal: RFValue(100),
+                fontSize: RFValue(40),
+                fontWeight: "bold",
+                color: "white",
               }}
-            />
-            <TouchableOpacity>
-              <Text
+            >
+              driveit
+            </Text>
+            <View
+              style={{
+                marginTop: RFValue(40),
+                justifyContent: "space-between",
+                flexDirection: "column",
+              }}
+            >
+              <TextInput
                 style={{
-                  color: "white",
-                  alignSelf: "center",
-                  marginTop: RFValue(10),
+                  backgroundColor: "white",
+                  width: RFValue(300),
+                  height: RFValue(50),
+                  borderRadius: RFValue(74),
+                  paddingHorizontal: RFValue(30),
+                  marginHorizontal: RFValue(100),
                 }}
-              >
-                Forgot Passworddd
-              </Text>
-            </TouchableOpacity>
-            <Button text={"LOG IN"} style={{ alignItems: "center" }} />
+              />
+              <TextInput
+                style={{
+                  backgroundColor: "white",
+                  width: RFValue(300),
+                  height: RFValue(50),
+                  marginTop: RFValue(30),
+                  borderRadius: RFValue(74),
+                  paddingHorizontal: RFValue(30),
+                  marginHorizontal: RFValue(100),
+                }}
+              />
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: "white",
+                    alignSelf: "center",
+                    marginTop: RFValue(10),
+                  }}
+                >
+                  Forgot Password
+                </Text>
+              </TouchableOpacity>
+              <Button text={"LOG IN"} style={{ alignItems: "center" }} />
+            </View>
           </View>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </LinearGradient>
     </ImageBackground>
   );
 }
